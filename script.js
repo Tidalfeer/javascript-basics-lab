4f228bf142548efb2c97687d97f5a73acb5d2659
Tidalfeer <tylerlee1406@gmail.com>
2024-09-20 23:01:30 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+
+var studentName = Frank;
+var studentAge = 25;
+var isEnrolled = true;
+
+console.log("Students Name:", studentName);
+console.log("Students Age:", studentAge);
+console.log("Is Enrolled:" , isEnrolled);
+
+let text = "Roll for intiative!";
+console.log("string:", text);
+
+let level = 20;
+let pi = 3.14159;
+console.log("Number (interger):", level);
+console.log("number (float):", pi);
+
+let arewehavingfun = true;
+console.log("Boolean:", arewehavingfun);
+
+let classes = ["Wizard","Cleric","Warlock","Bard"];
+console.log("Array:", classes);
+
+let person = {
+    firstname: "Frank",
+    lastname: "Stanley",
+    age: 412
+};
+console.log("Object:", person);
+
+let a = 20;
+let b = 10;
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+
+console.log("Greater than:", a > b);
+console.log("Less than:", b < a);
+console.log("Equal to:", a === 20);
+console.log("Not equal to:", a !== b);
+
+let x = false;
+let z = true;
+
+console.log("AND [&&]:", x && z);
+console.log("OR [||]:", x || z);
+console.log("NOT [!]:", !z);
+
